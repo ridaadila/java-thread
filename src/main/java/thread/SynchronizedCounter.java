a312659131a7a7d42f6a2421f65a7508011957b5
ridaadila <ridaadila10@gmail.com>
2022-10-13 11:38:38 +0700

    add synchronization of thread

---
+++ src/main/java/thread/SynchronizedCounter.java
@@ -0,0 +1,19 @@
+package thread;
+
+public class SynchronizedCounter {
+
+    private Long value = 0L;
+
+    public void increment()
+    {
+        synchronized (this)
+        {
+            value++;
+        }
+    }
+
+    public Long getValue()
+    {
+        return value;
+    }
+}
